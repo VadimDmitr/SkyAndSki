83eaf8f52b24b5b22531589fb762aaed484e3d8e
Sergey Sherstobitov <in43sh@gmail.com>
2023-03-27 11:29:23 -0700

    Implement Account desktop version.

--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { userDataContext } from "../../contexts/userContext";
-import { Accordion } from "../accordion/Accordion";
+import { AccountMobile } from "./account/AccountMobile";
 import { ChangePassword } from "../ChangePassword";
 import { DeleteAccount } from "../DeleteAccount";
 import { Logout } from "../Logout";
 
 export const Account = () => {
+    const [view, setView] = useState("order-history");
     const navigate = useNavigate();
     const {userData} = useContext(userDataContext);
 
@@ -16,24 +17,54 @@ export const Account = () => {
         // if (userData.token === null) navigate ("/");
     })
 
+    const changeView = (str) => {
+        switch (str) {
+            case "order-history":
+                setView("order-history");
+                break;
+
+            case "change-password":
+                setView("change-password");
+                break;
+
+            case "delete-account":
+                setView("delete-account");
+                break;
+
+            case "log-out":
+                setView("log-out");
+                break;
+
+            default:
+                setView("order-history");
+                break;
+        }
+    }
+
     if (userData) return (
-        <section className="account wrapper">
-            {/* {userData.token === "" ? <></> : <h1>Account</h1>} */}
-            {/* {userData.token === null ? <></> : <h1>{JSON.stringify(userData, null, 4)}</h1>} */}
-            {/* {userData.token === null ? <></> : <h1>User name: {userData.user.name} <br/> Token: {userData.token}</h1>} */}
-            <h1 className="account__greeting">Hello, {userData.user.name}</h1>
-            <Accordion title="Order history">
-                <p>123</p>
-            </Accordion>
-            <Accordion title="Change password">
-                <ChangePassword />
-            </Accordion>
-            <Accordion title="Delete account">
-                <DeleteAccount />
-            </Accordion>
-            <Accordion title="Log out">
-                <Logout />
-            </Accordion>
+        <section className="wrapper">
+            <div className="account mobile">
+                <AccountMobile />
+            </div>
+            <div className="account desktop">
+                <div className="account__sidebar">
+                    <h1 className="account__greeting">Hello, {userData.user.name}</h1>
+                    <p onClick={() => changeView("order-history")}>Order History</p>
+                    <p onClick={() => changeView("change-password")}>Change password</p>
+                    <p onClick={() => changeView("delete-account")}>Delete account</p>
+                    <p onClick={() => changeView("log-out")}>Log out</p>
+                </div>
+                <div className="account__switch-section desktop">
+                    { view === "order-history" ?
+                        <div>order history</div> :
+                            view === "change-password" ?
+                                <ChangePassword /> :
+                                view === "delete-account" ?
+                                    <DeleteAccount /> :
+                                        <Logout />
+                    }
+                </div>
+            </div>
         </section>
     )
     return (<></>)
---
+++ src/components/pages/account/AccountMobile.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { userDataContext } from "../../../contexts/userContext";
+import { Accordion } from "../../accordion/Accordion";
+import { ChangePassword } from "../../ChangePassword";
+import { DeleteAccount } from "../../DeleteAccount";
+import { Logout } from "../../Logout";
+
+export const AccountMobile = () => {
+    const navigate = useNavigate();
+    const {userData} = useContext(userDataContext);
+
+    useEffect(() => {
+        console.log("userData =====> ", userData);
+        if (userData === null) navigate ("/");
+        // if (userData.token === null) navigate ("/");
+    })
+
+    if (userData) return (
+        <section className="account mobile wrapper">
+            {/* {userData.token === "" ? <></> : <h1>Account</h1>} */}
+            {/* {userData.token === null ? <></> : <h1>{JSON.stringify(userData, null, 4)}</h1>} */}
+            {/* {userData.token === null ? <></> : <h1>User name: {userData.user.name} <br/> Token: {userData.token}</h1>} */}
+            <h1 className="account__greeting">Hello, {userData.user.name}</h1>
+            <Accordion title="Order history">
+                <p>123</p>
+            </Accordion>
+            <Accordion title="Change password">
+                <ChangePassword />
+            </Accordion>
+            <Accordion title="Delete account">
+                <DeleteAccount />
+            </Accordion>
+            <Accordion title="Log out">
+                <Logout />
+            </Accordion>
+        </section>
+    )
+    return (<></>)
+}
+
+// userData.token === null ? <></> : <h1>{JSON.stringify(userData, null, 4)}</h1>
