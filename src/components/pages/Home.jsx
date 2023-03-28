dfa0b7432ed0da8eae5a4ad816a4f1b4598fdde0
Sergey Sherstobitov <in43sh@gmail.com>
2023-03-28 13:32:39 -0700

    Start working on Catalog in Home component.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,17 @@
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div className="wrapper">
+            {/* not sure if i should use h1, h2, or h3 here */}
+            <h3>Our Most Popular Board category</h3>
+            <div className="filters">
+                <p>All</p>
+                <p>Ski</p>
+                <p>Boards</p>
+                <p>Other</p>
+            </div>
+            <div className="products">
+                <div className="product"></div>
+            </div>
+        </div>
     )
 }
