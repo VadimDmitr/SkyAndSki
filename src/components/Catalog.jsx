d4422d038eae941c74a46187044d50dcd37c5a46
Sergey Sherstobitov <in43sh@gmail.com>
2023-03-31 01:01:25 -0700

    Start working on catalog.

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,5 +1,19 @@
 export const Catalog = () => {
     return (
-        <div>catalog</div>
+        <div className="catalog">
+            <hr />
+            <p>Catalog</p>
+            <h2>Our Most Popular Board category</h2>
+            <div className="catalog__filters">
+                <p>All</p>
+                <p>Sky</p>
+                <p>Ski</p>
+                <p>Boards</p>
+                <p>Other</p>
+            </div>
+            <div className="products">
+                <div className="product"></div>
+            </div>
+        </div>
     )
 }
