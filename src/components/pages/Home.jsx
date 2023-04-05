5774bdc1c156079118343bce2d3cf79dc8dcf2ec
VadimDmitr <vadzzimluch@gmail.com>
2023-04-05 14:35:43 -0400

    Align contect in our features

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,19 @@
 import { OurUniqueFeatures } from "../OurUniqueFeatures";
-import { Catalog } from "../Catalog";
+import { Catalog } from "../Catalog"; 
 
 export const Home = () => {
 	return (
-	<>
-    <div className="our-unique-features">
-			<OurUniqueFeatures />
-		</div>
+		<>
+			<div className="our-unique-features">
+				<OurUniqueFeatures />
+			</div>
 
-		<div className="wrapper">
-			{/* not sure if i should use h1, h2, or h3 here */}
-			<Catalog />
-		</div>
-	</>
+			<div className="wrapper">
+				{/* not sure if i should use h1, h2, or h3 here */}
+				<Catalog />
+			</div>
+			
+		</>
 	);
 };
+
