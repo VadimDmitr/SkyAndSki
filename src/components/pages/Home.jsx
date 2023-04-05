664affbc549a501acf57a870c725b47fd42ea20f
VadimDmitr <vadzzimluch@gmail.com>
2023-04-05 12:39:53 -0400

    Add Our features services component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,17 @@
+import { OurUniqueFeatures } from "../OurUniqueFeatures";
 import { Catalog } from "../Catalog";
 
 export const Home = () => {
 	return (
+	<>
+    <div className="our-unique-features">
+			<OurUniqueFeatures />
+		</div>
+
 		<div className="wrapper">
 			{/* not sure if i should use h1, h2, or h3 here */}
 			<Catalog />
 		</div>
+	</>
 	);
 };
