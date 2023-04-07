16d1044e3d49a124df7d587f618441e6f81d3874
Sergey Sherstobitov <in43sh@gmail.com>
2023-04-07 14:28:54 -0700

    Add addItemToCart function to the Product component.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,63 @@
-import { userDataContext } from "../contexts/userContext"
+import { useContext } from "react";
+import { userDataContext } from "../contexts/userContext";
 import snowboard from "../images/snowboard_light.png";
 
 export const Product = () => {
+	const { cart, setCart } = useContext(userDataContext);
+
+	const item = {
+		id: "0",
+		title: "Sardinia White",
+		category: "board",
+		price: "312.49 SAR",
+		img: snowboard,
+	};
+
+	let cartLocal = {};
+
+	const addItemToCart = (item, quantity) => {
+		// Check if the item already exists in the cart
+		if (cartLocal[item.name]) {
+			// If the item exists, update its quantity
+			cartLocal[item.name].quantity += quantity;
+		} else {
+			// If the item doesn't exist, add it to the cartLocal with the specified quantity
+			cartLocal[item.name] = { ...item, quantity };
+		}
+		console.log("cartLocal ====> ", cartLocal);
+		setCart({...cart, cartLocal});
+		console.log("cart ====> ", cart);
+	};
+
 	return (
 		<div className="wrapper">
-            <div className="product">
-			<img className="product__img" src={snowboard} alt="snowboard" />
-			<div className="product__content">
-				<p className="product__title">Sardinia White</p>
-				<p className="product__price">312.49 SAR</p>
-				<p>description</p>
-                <div className="product__change-quantity-container">
-                    <span className="product__change-quantity product__change-quantity--left pointer">-</span>
-                    <span className="product__quantity pointer">1</span>
-                    <span className="product__change-quantity product__change-quantity--right pointer">+</span>
-                </div>
-                <p>Add to cart</p>
+			<div className="product">
+				<img
+					className="product__img"
+					src={snowboard}
+					alt="snowboard"
+				/>
+				<div className="product__content">
+					<p className="product__title">Sardinia White</p>
+					<p className="product__price">312.49 SAR</p>
+					<p>description</p>
+					<div className="product__change-quantity-container">
+						<span className="product__change-quantity product__change-quantity--left pointer">
+							-
+						</span>
+						<span className="product__quantity pointer">
+							1
+						</span>
+						<span
+							className="product__change-quantity product__change-quantity--right pointer"
+							onClick={() => addItemToCart(item, 1)}
+						>
+							+
+						</span>
+					</div>
+					<p>Add to cart</p>
+				</div>
 			</div>
 		</div>
-        </div>
 	);
 };
