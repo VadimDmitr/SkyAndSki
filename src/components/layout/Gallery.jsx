3fc81ed032e1bac05176eba16e32d4e13be0b259
VadimDmitr <vadzzimluch@gmail.com>
2023-04-12 20:58:20 -0400

    Import images

--- src/components/layout/Gallery.jsx
+++ src/components/layout/Gallery.jsx
@@ -1,43 +1,66 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import snowboarderDescending from "images/snowboarder_descending.png";
+import snowboarderWhiteJacket from "images/snowboarder_white_jacket.png";
+import snowboarderYellowJacket from "images/snowboarder_yellow_jacket.png";
 
 export const Gallery = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+	};
 
-  return (
-    <div>
-      <div className="carousel">
-        <Slider {...settings}>
-          <div>
-            <img src="image1.jpg" alt="Image 1" className="carousel__image" />
-          </div>
-          <div>
-            <img src="image2.jpg" alt="Image 2" className="carousel__image" />
-          </div>
-          <div>
-            <img src="image3.jpg" alt="Image 3" className="carousel__image" />
-          </div>
-        </Slider>
-      </div>
-      <div className="laptop-images">
-        <h2 className="laptop-images__title">Надпись</h2>
-        <img src="image1.jpg" alt="Image 1" className="laptop-images__image laptop-images__image--1" />
-        <img src="image2.jpg" alt="Image 2" className="laptop-images__image laptop-images__image--2" />
-        <img src="image3.jpg" alt="Image 3" className="laptop-images__image laptop-images__image--3" />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="carousel">
+      <p className="laptop-images__title">Our Gallery</p>
+				<Slider {...settings}>
+					<div>
+						<img
+							src={snowboarderDescending}
+							alt="Snowboarder descending"
+							className="carousel__image"
+						/>
+					</div>
+					<div>
+						<img
+							src={snowboarderWhiteJacket}
+							alt="Snowboarder White Jacket"
+							className="carousel__image"
+						/>
+					</div>
+					<div>
+						<img
+							src={snowboarderYellowJacket}
+							alt="Snowboarder Yellow Jacket"
+							className="carousel__image"
+						/>
+					</div>
+				</Slider>
+			</div>
+			<div className="laptop-images">
+				<p className="laptop-images__title">Our Gallery</p>
+				<img
+					src={snowboarderDescending}
+          alt="Snowboarder descending"
+					className="laptop-images__image laptop-images__image--1"
+				/>
+				<img
+					src={snowboarderWhiteJacket}
+          alt="Snowboarder White Jacket"
+					className="laptop-images__image laptop-images__image--2"
+				/>
+				<img
+					src={snowboarderYellowJacket}
+          alt="Snowboarder Yellow Jacket"
+					className="laptop-images__image laptop-images__image--3"
+				/>
+			</div>
+		</div>
+	);
 };
-
-
-
-
