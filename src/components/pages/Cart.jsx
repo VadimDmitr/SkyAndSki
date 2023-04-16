9dd9d711c596a4ffeae1454b0057b8cc277487ce
Sergey Sherstobitov <in43sh@gmail.com>
2023-04-15 17:45:46 -0700

    Render cart items in Cart components.

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -6,16 +6,8 @@ import cross from "images/icons/cross.svg";
 export const Cart = () => {
 	const { cart, setCart } = useContext(cartDataContext);
 
-	// const item = {
-	// 	id: "0",
-	// 	title: "Sardinia White",
-	// 	category: "board",
-	// 	price: "312.49 SAR",
-	// 	img: snowboard,
-	// };
-
 	const getCartValue = () => {
-		console.log("cart =====> ", cart);
+		console.log("cart inside getCartValue =====> ", cart);
 	};
 
 	const increaseQuantity = (item, quantity) => {
@@ -42,57 +34,47 @@ export const Cart = () => {
 				setCart(updatedCart);
 			}
 		}
-
-		// console.log("cart before setCart ====> ", cart);
-		// setCart({ ...cart, ...cart });
 		console.log("cart after setCart ====> ", cart);
 	};
-
-	const cartEntries = Object.entries(cart).map(
-		(item, i) => {
-			<p>{item.productName}</p>;
-			<p>{JSON.stringify(item)}</p>
-		}
-	);
 
 	return (
 		<div className="wrapper">
 			<div className="cart">
 				<h2>Cart</h2>
 				<div className="cart__items">
-					{/* {cart.map((item) => (
-					))} */}
-					{cartEntries}
-					{/* <div className="cart-item">
-						<img
-							className="cart-item__img"
-							src={snowboard}
-							alt="snowboard"
-						/>
-						<div className="cart-item__content">
-							<p className="cart-item__title">
-								{item.productName}
-							</p>
+					{cart.map((item) => (
+						<div className="cart-item">
 							<img
-								src={cross}
-								alt="remove item from cart"
+								className="cart-item__img"
+								src={snowboard}
+								alt="snowboard"
 							/>
-							<p>120$</p>
-							<div className="cart-item">
-								<p className="pointer">-</p>
-								<p>1</p>
-								<p>{cart["Sardinia White"].cart.quantity}</p>
-								<p
-									className="pointer"
-									// onClick={() =>
-									// 	increaseQuantity(item, 1)
-									// }
-								>
-									+
+							<div className="cart-item__content">
+								<p className="cart-item__title">
+									{item.productName}
 								</p>
+								<img
+									src={cross}
+									alt="remove item from cart"
+								/>
+								<p>120$</p>
+								<div className="cart-item">
+									<p className="pointer">-</p>
+									<p>
+										{item.quantity}
+									</p>
+									<p
+										className="pointer"
+										onClick={() =>
+											increaseQuantity(item, 1)
+										}
+									>
+										+
+									</p>
+								</div>
 							</div>
 						</div>
-					</div> */}
+					))}
 				</div>
 				<button onClick={() => getCartValue()}>
 					Click
