d26a3ac1c64a193a3bfdcff091a18bebb1373706
VadimDmitr <vadzzimluch@gmail.com>
2023-04-16 20:20:14 -0400

    Add style elements

--- src/components/layout/WhyChooseUs.jsx
+++ src/components/layout/WhyChooseUs.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import tesla from "images/icons/why_choose_us/tesla.svg";
 import docusign from "images/icons/why_choose_us/docu_sign.svg";
@@ -8,111 +9,80 @@ import discord from "images/icons/why_choose_us/discord.svg";
 import whyUs from "images/why_choose_us.png";
 
 export const WhyChooseUs = () => {
-	return (
+  return (
 	<div className="why-choose-us-container">
-		<div className="company-icons">
-			<div>
-				<img
-					src={tesla}
-					alt="Tesla"
-					className="company-items__tesla"
-				/>
-			</div>
-			<div>
-				<img
-					src={docusign}
-					alt="DocuSign"
-					className="company-items__docusign"
-				/>
-			</div>
-			<div>
-				<img
-					src={maze}
-					alt="Maze"
-					className="company-items__maze"
-				/>
-			</div>
-			<div>
-				<img
-					src={uber}
-					alt="Uber"
-					className="company-items__uber"
-				/>
-			</div>
-			<div>
-				<img
-					src={miro}
-					alt="Miro"
-					className="company-items__miro"
-				/>
-			</div>
-			<div>
-				<img
-					src={discord}
-					alt="Discord"
-					className="company-items__discord"
-				/>
-			</div>
-			<div className="middle-section">
-				<div>
-					<img
-						src={whyUs}
-						alt="Snowboarder"
-						className="middle-section__image"
-					/>
-				</div>
-				<div className="middle-section__title-text">
-					<div>
-						<h1 className="middle-section__title">
-							{" "}
-							Why choose US{" "}
-						</h1>
-					</div>
-					<div>
-						<h2 className="middle-section__text">
-							{" "}
-							Amet minim mollit non deserunt ullamco est sit
-							aliqua dolor do amet sint. Velit officia
-							consequat duis enim velit mollit.{" "}
-						</h2>
-					</div>
-				</div>{" "}
-				<NavLink
-					className="middle-section__link"
-					to={"/read more"}
-				>
-					<p>Read more</p>
-				</NavLink>
-				<div className="bottom-section__title-text">
-					<div>
-						<h1 className="bottom-section__title">
-							Service Repair
-						</h1>
-					</div>
-					<div>
-						<h2 className="bottom-section__text">
-							Amet minim mollit non deserunt ullamco est sit
-							aliqua dolor do amet sint. Velit officia
-							consequat duis enim velit mollit.
-						</h2>
-					</div>
-				</div>
-				<div className="bottom-section__title-text">
-					<div>
-						<h1 className="bottom-section__title">
-							Board Accessories
-						</h1>
-					</div>
-					<div>
-						<h2 className="bottom-section__text">
-							Amet minim mollit non deserunt ullamco est sit
-							aliqua dolor do amet sint. Velit officia
-							consequat duis enim velit mollit.
-						</h2>
-					</div>
-				</div>
-				<div className="bottom-section__title-text">
-					<div>
+      <div className="company-icons">
+        <div>
+          <img src={tesla} alt="Tesla" className="company-items__tesla" />
+        </div>
+        <div>
+          <img
+            src={docusign}
+            alt="DocuSign"
+            className="company-items__docusign"
+          />
+        </div>
+        <div>
+          <img src={maze} alt="Maze" className="company-items__maze" />
+        </div>
+        <div>
+          <img src={uber} alt="Uber" className="company-items__uber" />
+        </div>
+        <div>
+          <img src={miro} alt="Miro" className="company-items__miro" />
+        </div>
+        <div>
+          <img src={discord} alt="Discord" className="company-items__discord" />
+        </div>
+      </div>
+      <div className="middle-section">
+        <div className="middle-section__content">
+          <img
+            src={whyUs}
+            alt="Snowboarder"
+            className="middle-section__image"
+          />
+          <div className="middle-section__title-text">
+            <div>
+              <h1 className="middle-section__title">Why choose US</h1>
+            </div>
+            <div>
+              <h2 className="middle-section__text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+              </h2>
+            </div>
+          </div>
+        </div>
+        <NavLink className="middle-section__link" to={"/read more"}>
+          <p>Read more</p>
+        </NavLink>
+      </div>
+      <div className="bottom-section">
+        <div className="bottom-section__title-text">
+          <div>
+            <h1 className="bottom-section__title">Service Repair</h1>
+          </div>
+          <div>
+            <h2 className="bottom-section__text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </h2>
+          </div>
+        </div>
+        <div className="bottom-section__title-text">
+          <div>
+            <h1 className="bottom-section__title">Board Accessories</h1>
+          </div>
+          <div>
+            <h2 className="bottom-section__text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </h2>
+          </div>
+        </div>
+        <div className="bottom-section__title-text">
+          <div>
 						<h1 className="bottom-section__title">
 							Warranty & Guarantee
 						</h1>
@@ -127,6 +97,6 @@ export const WhyChooseUs = () => {
 				</div>
 			</div>
 		</div>
-	</div>
+
 	);
 };
