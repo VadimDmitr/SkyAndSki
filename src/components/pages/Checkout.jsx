c8bbc7c872e37748d5e234eb74814974eff90fcc
Sergey Sherstobitov <in43sh@gmail.com>
2023-04-18 17:55:25 -0700

    Work on address form in Checkout page: start styling inputs.

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -1,7 +1,25 @@
-// import { useContext } from "react";
+import { useState, useContext } from "react";
 // import { cartDataContext } from "contexts/cartContext";
 
 export const Checkout = () => {
+	const [state, setState] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		country: "",
+		zip: "",
+		city: "",
+		address: "",
+	});
+
+	function handleChange(evt) {
+		const value = evt.target.value;
+		setState({
+			...state,
+			[evt.target.name]: value,
+		});
+		console.log("state ===> ", state);
+	}
 	// const { cart } = useContext(cartDataContext);
 
 	// const estimate = cart.reduce(
@@ -19,29 +37,99 @@ export const Checkout = () => {
 				<div className="checkout__address-container">
 					<h2>1. Your Address</h2>
 					<form className="checkout__address-form">
-						<p>First Name</p>
-						<input />
-						<p>Last Name</p>
-						<input />
-						<p>Email</p>
-						<input />
-						<p>Country</p>
-						<input />
-						<p>Zip Code</p>
-						<input />
-						<p>City</p>
-						<input />
-						<p>Address</p>
-						<input />
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								First Name
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="firstName"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								Last Name
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="lastName"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								Email
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="email"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								Country
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="contry"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								Zip Code
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="zip"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								City
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="city"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
+						<div className="checkout__address-form-container">
+							<label className="checkout__address-form-label">
+								Address
+							</label>
+							<input
+								className="checkout__address-form-input"
+								required
+								type="text"
+								name="address"
+								onChange={(event) => handleChange(event)}
+							/>
+						</div>
 					</form>
 				</div>
 				<div className="checkout__payment-container">
 					<h2>2. Payment</h2>
 					<form className="checkout__payment-form">
 						<p>Card number</p>
-						<input />
+						<input required type="text" />
 						<p>Card holder</p>
-						<input />
+						<input required type="text" />
 						<p>Expiration date</p>
 						<p>CVV/CVC</p>
 						<p>HTTPS/SSL - Secure connection</p>
