666c41e6aed4a6cd0e9eb9111072719015e9c5fb
VadimDmitr <vadzzimluch@gmail.com>
2023-04-22 18:56:21 -0400

    Add elements

--- src/components/layout/ExploreOurProduct.jsx
+++ src/components/layout/ExploreOurProduct.jsx
@@ -1,2 +1,15 @@
-import snowboard from "images/snowboard_light.png";
-import goggles from "images/goggles.png";
+export const ExploreOurProduct = () => {
+	return (
+		<div className="explore-our-produc__container">
+			<h1 className="explore-our-product__title">
+				Explore Our Product
+			</h1>
+			<h2 className="explore-our-product__subtitle">
+				Amet minim mollit non deserunt ullamco est sit
+				aliqua dolor do amet sint. Velit officia consequat
+				duis enim velit mollit.
+			</h2>
+      {/*Products from catalog*/}
+		</div>
+	);
+};
