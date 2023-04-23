4c031ab2f27e80ff9d8bf2e28cbed12d73fcc0aa
VadimDmitr <vadzzimluch@gmail.com>
2023-04-23 14:55:08 -0400

    Merged TopScreen and Header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,13 +3,15 @@ import { TopScreen } from "components/TopScreen";
 import { Catalog } from "components/Catalog";
 import { Gallery } from "../layout/Gallery";
 import { WhyChooseUs } from "../layout/WhyChooseUs";
+import { Header } from "components/layout/header/Header";
 
 export const Home = () => {
 	return (
 		<>
-			<div className="top-screen">
-				<TopScreen />
-			</div>
+			<div className="header-top-screen-container">
+        <Header />
+        <TopScreen />
+      </div>
 			<div className="our-unique-features">
 				<OurUniqueFeatures />
 			</div>
