2ae218716639dafc93b4d28474ab15140758ab76
VadimDmitr <VadimDmitr@users.noreply.github.com>
2023-05-04 12:29:16 +0000

    Apply formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useLocation
+	BrowserRouter,
+	Routes,
+	Route,
+	useLocation,
 } from "react-router-dom";
 import React, { useEffect } from "react";
 import { Layout } from "components/Layout";
@@ -24,51 +24,51 @@ import { NotFound } from "components/pages/NotFound";
 import { ReadMore } from "components/pages/ReadMore";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-  return null;
+	return null;
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/read more" element={<ReadMore />} />
-          <Route
-            path="/product/:id"
-            element={<Product />}
-          />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/delete-account"
-            element={<DeleteAccount />}
-          />
-          <Route
-            path="/change-password"
-            element={<ChangePassword />}
-          />
-          <Route path="/order" element={<Order />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/account" element={<Account />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/read more" element={<ReadMore />} />
+					<Route
+						path="/product/:id"
+						element={<Product />}
+					/>
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/logout" element={<Logout />} />
+					<Route path="/register" element={<Register />} />
+					<Route
+						path="/delete-account"
+						element={<DeleteAccount />}
+					/>
+					<Route
+						path="/change-password"
+						element={<ChangePassword />}
+					/>
+					<Route path="/order" element={<Order />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,29 +4,34 @@ import snowboards from "images/about_snowboards.jpg";
 import together from "images/about_together.jpg";
 
 export const About = () => {
-  return (
-    <div className="wrapper">
-      <div className="about">
-        <h1 className="about__title">About Sky & Ski</h1>
+	return (
+		<div className="wrapper">
+			<div className="about">
+				<h1 className="about__title">About Sky & Ski</h1>
 				<div className="about__content">
-          <img src={jump} alt="Jump" />
-        <h2 className="about__subtitle">
-          Sky & Ski is a leading eCommerce platform for winter sports enthusiasts...
-        </h2>
-        <img src={descend} alt="Descend" />
-          <h2 className="about__subtitle">
-            Our mission is to help our customers enjoy the winter season...
-          </h2>
+					<img src={jump} alt="Jump" />
+					<h2 className="about__subtitle">
+						Sky & Ski is a leading eCommerce platform for
+						winter sports enthusiasts...
+					</h2>
+					<img src={descend} alt="Descend" />
+					<h2 className="about__subtitle">
+						Our mission is to help our customers enjoy the
+						winter season...
+					</h2>
 					<img src={snowboards} alt="Snowboards" />
-          <h2 className="about__subtitle">
-            We are dedicated to providing an exceptional shopping experience...
-          </h2>
-          <img src={together} alt="Together" />
-          <h2 className="about__subtitle">
-            Thank you for choosing Sky & Ski with our Team!<br/> And we look forward to serving all your needs!
-          </h2>
-        </div>
-      </div>
-    </div>
-  );
+					<h2 className="about__subtitle">
+						We are dedicated to providing an exceptional
+						shopping experience...
+					</h2>
+					<img src={together} alt="Together" />
+					<h2 className="about__subtitle">
+						Thank you for choosing Sky & Ski with our Team!
+						<br /> And we look forward to serving all your
+						needs!
+					</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
