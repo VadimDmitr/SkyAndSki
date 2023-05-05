c9ffbecd176c84b29d8278c7fe2dfcc870495f2f
VadimDmitr <vadzzimluch@gmail.com>
2023-05-05 09:51:05 -0400

    Delete pictures

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -63,7 +63,7 @@ export const Footer = () => {
 								</ExternalLink>
 								<ExternalLink
 									className="footer__social-link4"
-									href="https://youtube.com/"
+									href="https://www.youtube.com/watch?v=gAGIyKpVsPw&t=66s"
 								>
 									<img
 										className="footer__social-img"
