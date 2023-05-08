8d23a1b98480b0acefe1ae7849b6087ca8a09101
Sergey Sherstobitov <in43sh@gmail.com>
2023-05-08 12:44:19 -0700

    Add Contact related css.

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,22 +1,21 @@
-import React from "react";
 import { MapSection } from "components/layout/MapSection";
 
-export class Contact extends React.Component {
-	render() {
-		return (
-			<div className="map-section">
-				<div className="map-section__contact-info">
-					<h2 className="map-section__contact-title">
-						Contact Us
-					</h2>
-					<h3 className="map-section__adress">
-						Headquarters:
-						<br /> 201 W Main St STE 100, Durham, NC 27701
-						<br /> +1 314-999-9999
-					</h3>
-					<MapSection />
-				</div>
+export const Contact = () => {
+	return (
+		<div className="wrapper">
+			<div className="contact">
+				<h2 className="contact__contact-title">
+					Contact Us
+				</h2>
+				<h3 className="contact__address">
+					Headquarters:
+					<br /> 201 W Main St STE 100, Durham, NC 27701
+					<br /> +1 314-999-9999
+				</h3>
+				<MapSection />
+				{/* <div className="contact__contact-info">
+				</div> */}
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
