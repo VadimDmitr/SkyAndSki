ae96161d1eaaf521451b781d3e535e2a007b3da4
VadimDmitr <vadzzimluch@gmail.com>
2023-05-08 17:01:05 -0400

    Add Catalog route for Shop now button

--- src/components/layout/TopScreen.jsx
+++ src/components/layout/TopScreen.jsx
@@ -1,23 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import home from "images/upper-main.png";
 
 export const TopScreen = () => {
-	return (
-		<div className="top-screen">
-			<img
-				className="top-screen__main"
-				src={home}
-				alt="home"
-			/>
-			<div className="top-screen__text-and-button-container">
-				<h1 className="top-screen__skyski">SKY & SKI </h1>
-				<h2 className="top-screen__webuild">
-					We build only the best, customized to your
-					exacting demands
-				</h2>
-				<button className="top-screen__button">
-					SHOP NOW
-				</button>
-			</div>
-		</div>
-	);
+  const navigate = useNavigate();
+
+  const handleShopNowClick = () => {
+    navigate("/catalog");
+  };
+
+  return (
+    <div className="top-screen">
+      <img className="top-screen__main" src={home} alt="home" />
+      <div className="top-screen__text-and-button-container">
+        <h1 className="top-screen__skyski">SKY & SKI</h1>
+        <h2 className="top-screen__webuild">
+          We build only the best, customized to your exacting demands
+        </h2>
+        <button className="top-screen__button" onClick={handleShopNowClick}>
+          SHOP NOW
+        </button>
+      </div>
+    </div>
+  );
 };
