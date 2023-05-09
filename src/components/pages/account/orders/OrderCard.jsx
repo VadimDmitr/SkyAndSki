dc25f1de8179da8382f3bb99f05ea34e132d2916
Sergey Sherstobitov <in43sh@gmail.com>
2023-05-09 09:09:00 -0700

    Change many paddings and margins.

--- src/components/pages/account/orders/OrderCard.jsx
+++ src/components/pages/account/orders/OrderCard.jsx
@@ -4,7 +4,7 @@ export const OrderCard = ({ order }) => {
 	return (
 		<div className="order-card">
 			<p className="order-card__date-created">{order.createdAt.slice(0, 10)}</p>
-			<p className="order-card__total">{order.total}</p>
+			{/* <p className="order-card__total">{order.total}</p> */}
 			{/* <p>{order.products[0].title}</p> */}
 			{order.products.map((product) => (
 				<p className="order-card__product-title">{product.title}</p>
